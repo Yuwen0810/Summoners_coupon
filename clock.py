b9cc87ce46b20429c4b9a579094a7e338d4d274a
huangyuwen <ssd8661366@gmail.com>
2022-08-11 17:00:53 +0800

    20220811 17:00 - Add another caller at midnight

--- clock.py
+++ clock.py
@@ -10,11 +10,22 @@ def scheduled_job():
 	conn = urllib.request.urlopen(url)
 
 	print("=============== Awake Heroku start ===============")
+	print(f"url: {url}")
 	for key, value in conn.getheaders():
 		print(key, value)
 	print("=============== Awake Heroku end ===============")
 
-# scheduled_job()
+@sched.scheduled_job('cron', hour='16')
+def awake_heroku_caller():
+	url = "https://awake-each-heroku.herokuapp.com/"
+
+	conn = urllib.request.urlopen(url)
+	print("=============== Awake Heroku start ===============")
+	print(f"url: {url}")
+	for key, value in conn.getheaders():
+		print(key, value)
+	print("=============== Awake Heroku end ===============")
+
 sched.start()
 
 
